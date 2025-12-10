aad25a6d8b1ccacfe022b2bdcd4aff09e0566a5a
Tanay Dwivedi <tanaydwivedi678@gmail.com>
2025-12-10 09:48:31 +0530

    feature:Added Detailed features of Pokemon

--- PokeDex/app/index.tsx
+++ PokeDex/app/index.tsx
@@ -1,6 +1,12 @@
 import { useEffect, useState } from "react";
 import { ScrollView, Text, View } from "react-native";
 
+
+interface Pokemon {
+  name: string;
+  url: string;
+}
+
 export default function Index() {
   const [pokemons, setPokemons] = useState([]);
   useEffect(() => {
@@ -14,8 +20,19 @@ export default function Index() {
 
       const data = await response.json();
 
+      const detailedPokemons=await Promise.all(data.results.map(async (pokemon:Pokemon) => {
+        const res = await fetch(pokemon.url);
+        const details= await res.json();
+        return{
+          name: pokemon.name,
+          image: details.sprites.front_default, 
+        };
+      })
+      );
+      console.log(detailedPokemons);
+
       setPokemons(data.results);
-      console.log(data.results);
+      console.log(data);
     } catch (e) {
       console.log(e);
     }
