48aa67127344f7501ae748d1781a22a03c1001c9
Tanay Dwivedi <tanaydwivedi678@gmail.com>
2026-01-26 19:15:39 +0530

    fix:renamed the pages

--- PokeDex/app/_layout.tsx
+++ PokeDex/app/_layout.tsx
@@ -1,5 +1,20 @@
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack />;
+
+  return (
+    <Stack>
+    <Stack.Screen
+      name="index"
+      options={{
+        title: "Home",
+      }}/>
+    <Stack.Screen
+      name="detailed"
+      options={{
+        title: "Details",
+      }}/>
+    </Stack>
+  
+);
 }
